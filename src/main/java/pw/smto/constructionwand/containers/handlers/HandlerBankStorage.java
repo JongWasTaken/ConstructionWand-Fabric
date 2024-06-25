21a900fcb337f1b459eb461e0273bba542c0ff95
unilock <unilock@fennet.rentals>
2024-06-25 15:27:51 -0400

    Bank Storage support
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/pw/smto/constructionwand/containers/handlers/HandlerBankStorage.java
@@ -0,0 +1,48 @@
+package pw.smto.constructionwand.containers.handlers;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.natte.bankstorage.container.BankItemStorage;
+import net.natte.bankstorage.item.BankItem;
+import net.natte.bankstorage.item.CachedBankStorage;
+import net.natte.bankstorage.util.Util;
+import pw.smto.constructionwand.api.IContainerHandler;
+import pw.smto.constructionwand.basics.WandUtil;
+
+public class HandlerBankStorage implements IContainerHandler {
+    @Override
+    public boolean matches(PlayerEntity player, ItemStack itemStack, ItemStack inventoryStack) {
+        return inventoryStack != null && inventoryStack.getCount() == 1 && inventoryStack.getItem() instanceof BankItem;
+    }
+
+    @Override
+    public int countItems(PlayerEntity player, ItemStack itemStack, ItemStack inventoryStack) {
+        CachedBankStorage cachedBankStorage = CachedBankStorage.getBankStorage(inventoryStack);
+        if(cachedBankStorage == null) return 0;
+
+        return cachedBankStorage.blockItems.stream().filter(stack -> WandUtil.stackEquals(stack, itemStack)).map(ItemStack::getCount).reduce(0, Integer::sum);
+    }
+
+    @Override
+    public int useItems(PlayerEntity player, ItemStack itemStack, ItemStack inventoryStack, int count) {
+        BankItemStorage bankItemStorage = Util.getBankItemStorage(inventoryStack, player.getWorld());
+        if(bankItemStorage == null) return 0;
+
+        for(int i = 0; i < bankItemStorage.getItems().size(); i++) {
+            ItemStack handlerStack = bankItemStorage.getItems().get(i);
+            if(WandUtil.stackEquals(itemStack, handlerStack)) {
+                ItemStack extracted = bankItemStorage.removeStack(i, count);
+                count -= extracted.getCount();
+                if(count <= 0) break;
+            }
+        }
+
+        if(EnvType.CLIENT.equals(FabricLoader.getInstance().getEnvironmentType())) {
+            CachedBankStorage.requestCacheUpdate(Util.getUUID(inventoryStack));
+        }
+
+        return count;
+    }
+}
