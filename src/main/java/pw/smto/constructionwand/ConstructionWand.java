dfe6494a39ea3ea82be25612f78ce97e360fa4f2
JongWasTaken <jong@smto.pw>
2024-08-30 12:37:09 +0200

    Better 1.21.1 support, drop 1.21

--- src/main/java/pw/smto/constructionwand/ConstructionWand.java
+++ src/main/java/pw/smto/constructionwand/ConstructionWand.java
@@ -1,13 +1,14 @@
 package pw.smto.constructionwand;
 
-import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.impl.core.ForgeConfigSpecAdapter;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.util.Identifier;
-import net.minecraftforge.fml.config.ModConfig;
+import net.neoforged.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pw.smto.constructionwand.basics.ConfigClient;
@@ -20,8 +21,6 @@ import pw.smto.constructionwand.wand.undo.UndoHistory;
 public class ConstructionWand implements ModInitializer
 {
     public static final String MOD_ID = "constructionwand";
-    public static final String MODNAME = "ConstructionWand";
-
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static Identifier id(String name) {
@@ -33,16 +32,13 @@ public class ConstructionWand implements ModInitializer
         LOGGER.info("ConstructionWand says hello - may the odds be ever in your favor.");
         Registry.registerAll();
         ConfigServer.init();
-        ForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.SERVER, ConfigServer.SPEC);
-        ForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.CLIENT, ConfigClient.SPEC);
+        NeoForgeConfigRegistry.INSTANCE.register(MOD_ID, ModConfig.Type.SERVER, new ForgeConfigSpecAdapter(ConfigServer.SPEC));
         Network.init();
         ServerLifecycleEvents.SERVER_STARTED.register((MinecraftServer server) -> {
             ReplacementRegistry.init();
             ModCompat.checkForMods();
             ContainerManager.init();
         });
-        ServerPlayConnectionEvents.DISCONNECT.register((ServerPlayNetworkHandler handler, MinecraftServer server) -> {
-            UndoHistory.removePlayerEntity(handler.player);
-        });
+        ServerPlayConnectionEvents.DISCONNECT.register((ServerPlayNetworkHandler handler, MinecraftServer server) -> UndoHistory.removePlayerEntity(handler.player));
     }
 }
--- src/main/java/pw/smto/constructionwand/ConstructionWandClient.java
+++ src/main/java/pw/smto/constructionwand/ConstructionWandClient.java
@@ -1,5 +1,7 @@
 package pw.smto.constructionwand;
 
+import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.impl.core.ForgeConfigSpecAdapter;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -7,6 +9,8 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.neoforged.fml.config.ModConfig;
+import pw.smto.constructionwand.basics.ConfigClient;
 import pw.smto.constructionwand.basics.option.WandOptions;
 import pw.smto.constructionwand.client.ClientEvents;
 import pw.smto.constructionwand.client.Network;
@@ -18,6 +22,7 @@ import static pw.smto.constructionwand.Registry.Items.WANDS;
 public class ConstructionWandClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        NeoForgeConfigRegistry.INSTANCE.register(ConstructionWand.MOD_ID, ModConfig.Type.CLIENT, new ForgeConfigSpecAdapter(ConfigClient.SPEC));
         for(Item item : WANDS) {
             ModelPredicateProviderRegistry.register(
                     item, ConstructionWand.id("using_core"),
