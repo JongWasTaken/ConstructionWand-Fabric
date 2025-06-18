2cd1fc05b6f8bbf2044e1cb55d5f16e98f609d01
JongWasTaken <jong@smto.pw>
2025-06-18 23:36:40 +0200

    1.1.2: add key bind for OPT
    (cherry picked from commit 1905505dcce0bdf6708a704583562c1c0684125b)

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Construction Wand for Fabric
 
-This is an unofficial port of [ConstructionWand](https://github.com/Theta-Dev/ConstructionWand) by Theta-Dev for Minecraft 1.21.  
+This is an unofficial port of [ConstructionWand](https://github.com/Theta-Dev/ConstructionWand) by Theta-Dev for Minecraft 1.21+.  
 
 ## Downloads
 [![https://modrinth.com/mod/fabric-api](https://img.shields.io/badge/Requires_Fabric_API-white?style=for-the-badge&logo=modrinth&logoColor=black)](https://modrinth.com/mod/fabric-api)
-[![https://modrinth.com/mod/owo-lib](https://img.shields.io/badge/Requires_owo--lib-purple?style=for-the-badge&logo=modrinth&logoColor=black)](https://modrinth.com/mod/owo-lib)
   
 [Modrinth](https://modrinth.com/mod/construction-wand-fabric)  
 [CurseForge](https://curseforge.com/minecraft/mc-mods/construction-wand-fabric)
@@ -17,6 +16,7 @@ This project makes use of the following third-party libraries:
 - [forge-config-api-port](https://modrinth.com/mod/forge-config-api-port) (MPLv2 License), to keep config compatibility between both loaders (for now)
 - [Botania](https://github.com/VazkiiMods/Botania) (Custom License), for its API
 - [REI](https://github.com/shedaniel/RoughlyEnoughItems) (MIT License), for its API
+- Possibly more, if needed for cross-compat. 
 
 
 Just like the original release, this mod is licensed under the MIT license!
--- src/main/java/pw/smto/constructionwand/ConstructionWandClient.java
+++ src/main/java/pw/smto/constructionwand/ConstructionWandClient.java
@@ -1,25 +1,19 @@
 package pw.smto.constructionwand;
 
-import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
 import fuzs.forgeconfigapiport.fabric.impl.core.ForgeConfigSpecAdapter;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.neoforged.fml.config.ModConfig;
 import pw.smto.constructionwand.basics.ConfigClient;
-import pw.smto.constructionwand.basics.option.WandOptions;
 import pw.smto.constructionwand.client.ClientEvents;
 import pw.smto.constructionwand.client.Network;
-import pw.smto.constructionwand.items.wand.ItemWand;
-
-import static pw.smto.constructionwand.Registry.Items.WANDS;
 
 @Environment(EnvType.CLIENT)
 public class ConstructionWandClient implements ClientModInitializer {
+    public static KeyBinding optKey;
+
     @Override
     public void onInitializeClient() {
         NeoForgeConfigRegistry.INSTANCE.register(ConstructionWand.MOD_ID, ModConfig.Type.CLIENT, new ForgeConfigSpecAdapter(ConfigClient.SPEC));
@@ -40,6 +34,7 @@ public class ConstructionWandClient implements ClientModInitializer {
             }, item);
         }
         Network.init();
+        optKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.constructionwand.opt_key", InputUtil.Type.KEYSYM, ConfigClient.OPT_KEY.get(), "key.category.constructionwand"));
         ClientEvents.init();
     }
 }
